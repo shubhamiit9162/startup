ec9154944f7cc4657280264a4760a31d9c0139e0
shubhs <shubhamiit9162@gmial.com>
2024-12-16 12:31:26 +0530

    chanegs

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
 const authRoutes = require('./routes/authRoutes');
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 connectDB();
@@ -17,5 +18,5 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
